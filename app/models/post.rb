90b8c86247dece135ad093d2124b222f40ebc3d5
Desmond Yentumi <yentumid7@gmail.com>
2024-07-20 17:58:20 +0000

    Added image support to image model.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
-validates :title, presence: true, length: {minimum: 4, maximum: 50}
-validates :description, presence: true, length: {minimum: 5, maximum: 150}
-validates :keywords, presence: true, length: {minimum: 4, maximum: 50}
+  validates :title, presence: true, length: {minimum: 4, maximum: 50}
+  validates :description, presence: true, length: {minimum: 5, maximum: 150}
+  validates :keywords, presence: true, length: {minimum: 4, maximum: 50}
 
+  has_many_attached :images
 end
