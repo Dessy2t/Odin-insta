9bf21a12b1eb5f7bbf18a4fa9349109999cf7025
Desmond Yentumi <yentumid7@gmail.com>
2024-07-20 17:11:53 +0000

    Added validations to the Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+validates :title, presence: true, length: {minimum: 4, maximum: 50}
+validates :description, presence: true, length: {minimum: 5, maximum: 150}
+validates :keywords, presence: true, length: {minimum: 4, maximum: 50}
+
 end
